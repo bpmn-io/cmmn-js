5b7144b62cc6f937969f68f3e0d0191b915b0f56
pedesen <patrick.dehn@camunda.com>
2016-03-15 11:02:46 +0100

    chore(font): update cmmn-font
    Related to #24

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -4,6 +4,8 @@ var TestHelper = module.exports = require('./helper');
 
 TestHelper.insertCSS('diagram-js.css', require('diagram-js/assets/diagram-js.css'));
 
+TestHelper.insertCSS('cmmn-embedded.css', require('../assets/cmmn-font/css/cmmn-embedded.css'));
+
 TestHelper.insertCSS('diagram-js-testing.css',
   '.test-container .result { height: 500px; }' + '.test-container > div'
 );
