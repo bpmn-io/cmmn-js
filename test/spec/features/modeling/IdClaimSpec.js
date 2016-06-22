521e7f3b52b1126a8ae2a469efff0f19c85ccd50
pedesen <patrick.dehn@camunda.com>
2016-06-22 11:37:55 +0200

    chore(modeling): retain plan item id on replace
    Closes #94

--- test/spec/features/modeling/IdClaimSpec.js
+++ test/spec/features/modeling/IdClaimSpec.js
@@ -47,6 +47,65 @@ describe('features/modeling - id claim management', function() {
       expect(moddle.ids.assigned(id)).to.eql(moddleElement);
     }));
 
+
+    it('should unclaim id when removing case plan model', inject(function(elementRegistry, modeling, moddle) {
+      // given
+      var casePlanModel = elementRegistry.get('CasePlanModel_1');
+
+      // when
+      modeling.removeElements([ casePlanModel ]);
+
+      // then
+      expect(moddle.ids.assigned('CasePlanModel_1')).to.be.false;
+    }));
+
+
+    it('should revert unclaim action on restoring case plan model',
+      inject(function(elementRegistry, modeling, moddle, commandStack) {
+
+        // given
+        var casePlanModel = elementRegistry.get('CasePlanModel_1');
+
+        modeling.removeElements([ casePlanModel ]);
+
+        // when
+        commandStack.undo();
+
+        // then
+        expect(moddle.ids.assigned('CasePlanModel_1')).to.eql(casePlanModel.businessObject);
+      }
+    ));
+
+  });
+
+
+  describe('on replace plan item', function() {
+
+    it('should retain id - execute', inject(function(moddle, cmmnReplace) {
+
+      // when
+      var processTaskPI = cmmnReplace.replaceElement(element, { type: 'cmmn:PlanItem', definitionType: 'cmmn:ProcessTask' });
+
+      // then
+      // expect the new process task plan item to get the id of the replaced human task
+      expect(moddle.ids.assigned(id)).to.eql(processTaskPI.businessObject);
+
+    }));
+
+
+    it('should retain id - undo', inject(function(moddle, cmmnReplace, commandStack) {
+
+      // given
+      cmmnReplace.replaceElement(element, { type: 'cmmn:PlanItem', definitionType: 'cmmn:ProcessTask' });
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(moddle.ids.assigned(id)).to.eql(moddleElement);
+
+    }));
+
   });
 
 });
