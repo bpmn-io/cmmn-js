c68448ac6935762057b011bf6813eff06a240548
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-02-08 20:15:25 +0100

    chore(project): depend on cmmn-font
    * drop fonts served via assets in favor of
      dependency to cmmn-font
    
    BREAKING CHANGE:
    
    * assets/cmmn-font dependency has dropped; will
      be re-introduced as dist/assets/cmmn-font

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -2,9 +2,13 @@
 
 var TestHelper = module.exports = require('./helper');
 
-TestHelper.insertCSS('diagram-js.css', require('diagram-js/assets/diagram-js.css'));
+TestHelper.insertCSS('diagram-js.css',
+  require('diagram-js/assets/diagram-js.css')
+);
 
-TestHelper.insertCSS('cmmn-embedded.css', require('../assets/cmmn-font/css/cmmn-embedded.css'));
+TestHelper.insertCSS('cmmn-embedded.css',
+  require('cmmn-font/dist/css/cmmn-embedded.css')
+);
 
 TestHelper.insertCSS('diagram-js-testing.css',
   '.test-container .result { height: 500px; }' + '.test-container > div'
