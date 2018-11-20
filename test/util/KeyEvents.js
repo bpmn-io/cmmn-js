46e081fc48443434f9051b3a037818b3828e9025
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-11-20 16:40:43 +0100

    chore(keyboard): register shortcuts backed by editor actions only
    * only register keyboard bindings that are backed by
      actual editor actions
    * rename CmmnKeyBindings -> CmmnKeyboardBindings for
      parity with diagram-js@latest
    * sub-class and override _keyboardBindings_ service
      provided by diagram-js
    * extend spec to verify default actions

---
+++ test/util/KeyEvents.js
@@ -0,0 +1,12 @@
+function createKeyEvent(element, key, ctrlKey) {
+  var e = document.createEvent('Events') || new document.defaultView.CustomEvent('keyEvent');
+
+  e.key = key;
+  e.keyCode = key;
+  e.which = key;
+  e.ctrlKey = ctrlKey;
+
+  return e;
+}
+
+module.exports.createKeyEvent = createKeyEvent;
