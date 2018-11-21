c094cf7719b3a2ecc0ef9b0d29692062dad2c020
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-11-21 09:44:12 +0100

    test(keyboard): use standard KeyEvents util

--- test/util/KeyEvents.js
+++ test/util/KeyEvents.js
@@ -1,12 +1,23 @@
-function createKeyEvent(element, key, ctrlKey) {
-  var e = document.createEvent('Events') || new document.defaultView.CustomEvent('keyEvent');
+var isString = require('min-dash').isString;
+var assign = require('min-dash').assign;
 
-  e.key = key;
-  e.keyCode = key;
-  e.which = key;
-  e.ctrlKey = ctrlKey;
+/**
+ * Create a fake key event for testing purposes.
+ *
+ * @param {String|Number} key the key or keyCode/charCode
+ * @param {Object} [attrs]
+ *
+ * @return {Event}
+ */
+function createKeyEvent(key, attrs) {
+  var event = document.createEvent('Events') || new document.defaultView.CustomEvent('keyEvent');
 
-  return e;
+  // init and mark as bubbles / cancelable
+  event.initEvent('keydown', false, true);
+
+  var keyAttrs = isString(key) ? { key: key } : { keyCode: key, which: key };
+
+  return assign(event, keyAttrs, attrs || {});
 }
 
 module.exports.createKeyEvent = createKeyEvent;
